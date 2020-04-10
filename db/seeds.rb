5d27bad2e42b4824282db2af247b91ea876fb144
abhiyan <abhiyan@karyamitr.com>
2020-04-10 12:17:12 +0530

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,22 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+products = Product.create([
+                          { 
+                            url: "https://hamrobazaar.com/i2149405-house-on-sale-at-nayabazar-khushibun-town-planning.html", 
+                            title: "House On Sale At Nayabazar, Khushibun, Town Planning" ,
+                            price: 31000000,
+                            image_url: "https://cdn.hamrobazaar.com/2149405_04018c618ec71h82_large.jpg" ,    
+                            mobile_number: "9841558702",
+                            description: "<tr><td> A beautiful semi- furnished house situated at Khushibun town planning, a prime location, near by Thamel is on sale.<br>\n<br>\nSome features: <br>\n13000 lts. Underground water tank<br>\nSal wooden stairs.<br>\nParqueting all rooms  <br>\nFall-ceiling all rooms<br>\nParking for a light vehicle and 2-3 bikes.<br>\nCompound wall<br>\n<br>\nNote: Three bed rooms with 1 big master bed room. All bed rooms have attached bathroom, closet, Kitchen/dinning, Puja room, store and laundry room.<br>\n\t</td></tr>"
+                         },
+                         { 
+                            url: "https://hamrobazaar.com/i2148426-sony-wf1000xm3.html", 
+                            title: " Sony Wf-1000xm3" ,
+                            price: 26000,
+                            image_url: "https://cdn.hamrobazaar.com/2148426_032876dg29a969c6_large.jpg" ,    
+                            mobile_number: "9810390548",
+                            description: "<tr><td> Packed with advanced audio technology, the WF-1000XM3 noise-canceling earbuds not only deliver noise-free listening but also offer breathtaking sound quality.</td></tr>"
+                         }                          
+                          ])
+
